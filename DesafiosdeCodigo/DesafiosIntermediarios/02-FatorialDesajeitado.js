44ca2a262e18c3aa220aa91352220d5bcfb9ba7a
Thamyres Magalhães <pink.thamyres@gmail.com>
2022-10-21 19:21:52 -0300

    Solução final do Fatorial Desajeitado

--- DesafiosdeCodigo/DesafiosIntermediarios/02-FatorialDesajeitado.js
+++ DesafiosdeCodigo/DesafiosIntermediarios/02-FatorialDesajeitado.js
@@ -1,44 +1,62 @@
 let lines = gets().split("\n"); 
-
 let n = parseInt(lines.shift()); 
 
-
 if (n <= 2) { 
-  
-  n * 1;
-  print(n); 
 
-} else {
-  
-  let res = 0;
-  let cnt = 1;
-  let tmp = n;
-  
-  n--;
+ print(n); 
 
-  while (n > 0) {
-
-
-  tmp *= n;
-  n--;
-
-  if(n != 0){
-    tmp /= n;
-    n--;
-  }
-
-  tmp += n;
-  n--;
-
-  res += tmp;
-
-  tmp = -n;
-
-  if (n == 1 ){
-    res += tmp;
-  }   
-
-  n--;
-}
-  print(res.toFixed(0));
 } 
+
+let i = n;
+let arr = []
+let total = 0
+
+
+while (i >= 1) {
+    arr[i] = i
+    i--
+}
+arr.shift()
+let array = arr.reverse()
+let a = array.length
+
+
+while (array.length > 0) {
+    if (array.length >= 4) {
+        total += parseInt((array[0] * array[1]) / (array[2])) + array[3]
+        array.splice(0, 4)        
+        while (array.length >= 4) {
+            total = total - parseInt((array[0] * array[1]) / array[2]) + array[3]
+            array.splice(0, 4)
+        }
+        if (array.lengh == 3) {
+            total = total - parseInt((array[0] * array[1]) / array[2])
+            array.splice(0, 3)
+        }
+        if (array.length == 2) {
+            total = total - (array[0] * array[1])
+            array.splice(0, 2)
+        }
+        if (array.length == 1) {
+            total = total - array[0]
+            array.splice(0, 1)
+        }
+    }
+    if (array.lengh == 3) {
+        total = total + parseInt((array[0] * array[1]) / array[2])
+        array.splice(0, 3)
+        if (array.length == 2) {
+            total = total - (array[0] * array[1])
+            array.splice(0, 2)
+        }
+        if (array.length == 1) {
+            total = total - array[0]
+            array.splice(0, 1)
+        }
+    }
+    if (array.length == 2) {
+        total = total - (array[0] * array[1])
+        array.splice(0, 2)
+    }
+}
+console.log(parseInt(total))
